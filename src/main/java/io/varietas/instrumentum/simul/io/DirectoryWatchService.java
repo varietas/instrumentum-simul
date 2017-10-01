17612bbbf2e1a7ccda2e4c8f0a11f020a487e176
Michael Rhöse <michael.rhoese@varietas.io>
2017-10-01 11:31:56 +0200

    IO package for folder watching added.

---
+++ src/main/java/io/varietas/instrumentum/simul/io/DirectoryWatchService.java
@@ -0,0 +1,107 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2015, Hindol Adhya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.varietas.instrumentum.simul.io;
+
+import java.io.IOException;
+import java.nio.file.WatchEvent;
+
+/**
+ * <h2>DirectoryWatchService</h2>
+ *
+ * Interface definition of a simple directory watch service.
+ *
+ * Implementations of this interface allow interested parties to <em>listen</em>
+ * to file system events coming from a specific directory.
+ *
+ * @author Hindol Adhya
+ * @author Michael Rhöse
+ * @version 1.0.0, 9/4/2015
+ */
+public interface DirectoryWatchService extends Service {
+
+    @Override
+    void start();
+
+    /* Suppress Exception */
+    /**
+     * Notifies the implementation of <em>this</em> interface that <code>dirPath</code> should be monitored for file system events. If the changed file matches any of the <code>globPatterns</code>,
+     * <code>listener</code> should be notified.
+     *
+     * @param listener The listener.
+     * @param dirPath The directory path.
+     * @param globPatterns Zero or more file patterns to be matched against file names. If none provided, matches <em>any</em> file.
+     * @throws IOException If <code>dirPath</code> is not a directory.
+     */
+    void register(OnFileChangeListener listener, String dirPath, String... globPatterns) throws IOException;
+
+    /* Suppress Exception */
+    /**
+     * Notifies the implementation of <em>this</em> interface that <code>dirPath</code> should be monitored for file system events. If the changed file matches any of the <code>globPatterns</code>,
+     * <code>listener</code> should be notified.
+     *
+     * Possible patterns:
+     * <ul>
+     * <li>E.g. "*.log"</li>
+     * <li>E.g. "input-?.txt"</li>
+     * <li>E.g. "config.ini"</li>
+     * <li>As many patterns as you like</li>
+     * </ul>
+     *
+     * @param listener The listener.
+     * @param dirPath The directory path.
+     * @param events The events which are registered for the directory.
+     * @param globPatterns Zero or more file patterns to be matched against file names. If none provided, matches <em>any</em> file.
+     * @throws IOException If <code>dirPath</code> is not a directory.
+     */
+    void register(OnFileChangeListener listener, String dirPath, WatchEvent.Kind[] events, String... globPatterns) throws IOException;
+
+    /**
+     * Interface definition for a callback to be invoked when a file under watch is changed.
+     */
+    interface OnFileChangeListener {
+
+        /**
+         * Called when the file is created.
+         *
+         * @param filePath The file path.
+         */
+        default void onFileCreate(String filePath) {
+        }
+
+        /**
+         * Called when the file is modified.
+         *
+         * @param filePath The file path.
+         */
+        default void onFileModify(String filePath) {
+        }
+
+        /**
+         * Called when the file is deleted.
+         *
+         * @param filePath The file path.
+         */
+        default void onFileDelete(String filePath) {
+        }
+    }
+}
---
+++ src/main/java/io/varietas/instrumentum/simul/io/SimpleDirectoryWatchService.java
@@ -0,0 +1,300 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2015, Hindol Adhya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.varietas.instrumentum.simul.io;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+/**
+ * <h2>SimpleDirectoryWatchService</h2>
+ *
+ * <p>
+ * A simple class which can monitor files and notify interested parties (i.e. listeners) of file changes.</p>
+ *
+ * <p>
+ * This class is kept lean by only keeping methods that are actually being called.</p>
+ *
+ * <u>Usage</u>
+ * <pre>
+ * <code>DirectoryWatchService watchService = new SimpleDirectoryWatchService(); // May throw
+ *  watchService.register( // May throw
+ *      new DirectoryWatchService.OnFileChangeListener() {
+ *          {@literal @}Override
+ *          public void onFileCreate(String filePath) {
+ *              System.out.println("File " + filePath + " created.");
+ *          }
+ *
+ *          {@literal @}Override
+ *          public void onFileModify(String filePath) {
+ *              System.out.println("File " + filePath + " modified.");
+ *          }
+ *
+ *          {@literal @}Override
+ *          public void onFileDelete(String filePath) {
+ *              System.out.println("File " + filePath + " deleted.");
+ *          }
+ *      },
+ *      "c:\\plugins",
+ *      new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE}
+ *  );
+ *
+ *  watchService.start();
+ * </code>
+ * </pre>
+ *
+ * @author Hindol Adhya
+ * @author Michael Rhöse
+ * @version 1.0.0, 9/4/2015
+ */
+public class SimpleDirectoryWatchService implements DirectoryWatchService, Runnable {
+
+    private final WatchService watchService;
+    private final AtomicBoolean isRunning;
+    private final ConcurrentMap<WatchKey, Path> watchKeyToDirPathMap;
+    private final ConcurrentMap<Path, Set<OnFileChangeListener>> dirPathToListenersMap;
+    private final ConcurrentMap<OnFileChangeListener, Set<PathMatcher>> listenerToFilePatternsMap;
+
+    /**
+     * A simple no argument constructor for creating a <code>SimpleDirectoryWatchService</code>.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
+    public SimpleDirectoryWatchService() throws IOException {
+        watchService = FileSystems.getDefault().newWatchService();
+        isRunning = new AtomicBoolean(false);
+        watchKeyToDirPathMap = newConcurrentMap();
+        dirPathToListenersMap = newConcurrentMap();
+        listenerToFilePatternsMap = newConcurrentMap();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>) event;
+    }
+
+    private static <K, V> ConcurrentMap<K, V> newConcurrentMap() {
+        return new ConcurrentHashMap<>();
+    }
+
+    private static <T> Set<T> newConcurrentSet() {
+        return Collections.newSetFromMap(newConcurrentMap());
+    }
+
+    public static PathMatcher matcherForGlobExpression(final String globPattern) {
+        return FileSystems.getDefault().getPathMatcher("glob:" + globPattern);
+    }
+
+    public static boolean matches(final Path input, final PathMatcher pattern) {
+        return pattern.matches(input);
+    }
+
+    public static boolean matchesAny(final Path input, final Set<PathMatcher> patterns) {
+        for (PathMatcher pattern : patterns) {
+            if (matches(input, pattern)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Path getDirPath(final WatchKey key) {
+        return watchKeyToDirPathMap.get(key);
+    }
+
+    private Set<OnFileChangeListener> getListeners(final Path dir) {
+        return dirPathToListenersMap.get(dir);
+    }
+
+    private Set<PathMatcher> getPatterns(final OnFileChangeListener listener) {
+        return listenerToFilePatternsMap.get(listener);
+    }
+
+    private Set<OnFileChangeListener> matchedListeners(final Path dir, final Path file) {
+        return getListeners(dir)
+            .stream()
+            .filter(listener -> matchesAny(file, getPatterns(listener)))
+            .collect(Collectors.toSet());
+    }
+
+    private void notifyListeners(final WatchKey key) {
+        for (WatchEvent<?> event : key.pollEvents()) {
+            WatchEvent.Kind eventKind = event.kind();
+
+            // Overflow occurs when the watch event queue is overflown
+            // with events.
+            if (eventKind.equals(StandardWatchEventKinds.OVERFLOW)) {
+                // TODO: Notify all listeners.
+                return;
+            }
+
+            WatchEvent<Path> pathEvent = cast(event);
+            Path file = pathEvent.context();
+
+            if (eventKind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
+                matchedListeners(getDirPath(key), file)
+                    .forEach(listener -> listener.onFileCreate(file.toString()));
+                continue;
+            }
+            if (eventKind.equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
+                matchedListeners(getDirPath(key), file)
+                    .forEach(listener -> listener.onFileModify(file.toString()));
+                continue;
+            }
+            if (eventKind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+                matchedListeners(getDirPath(key), file)
+                    .forEach(listener -> listener.onFileDelete(file.toString()));
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void register(final OnFileChangeListener listener, final String dirPath, final String... globPatterns) throws IOException {
+
+        this.register(
+            listener,
+            dirPath,
+            new WatchEvent.Kind[]{StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE},
+            globPatterns);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void register(final OnFileChangeListener listener, final String dirPath, final WatchEvent.Kind[] events, final String... globPatterns) throws IOException {
+        Path dir = Paths.get(dirPath);
+
+        if (!Files.isDirectory(dir)) {
+            throw new IllegalArgumentException(dirPath + " is not a directory.");
+        }
+
+        if (!dirPathToListenersMap.containsKey(dir)) {
+            // May throw
+            WatchKey key = dir.register(watchService, events);
+
+            watchKeyToDirPathMap.put(key, dir);
+            dirPathToListenersMap.put(dir, newConcurrentSet());
+        }
+
+        getListeners(dir).add(listener);
+
+        Set<PathMatcher> patterns = newConcurrentSet();
+
+        for (String globPattern : globPatterns) {
+            patterns.add(matcherForGlobExpression(globPattern));
+        }
+
+        if (patterns.isEmpty()) {
+            patterns.add(matcherForGlobExpression("*")); // Match everything if no filter is found
+        }
+
+        listenerToFilePatternsMap.put(listener, patterns);
+
+        System.out.println("Watching files matching " + Arrays.toString(globPatterns)
+            + " under " + dirPath + " for changes.");
+    }
+
+    /**
+     * Start this <code>SimpleDirectoryWatchService</code> instance by spawning a new thread.
+     *
+     * @see #stop()
+     */
+    @Override
+    public void start() {
+        if (isRunning.compareAndSet(false, true)) {
+            Thread runnerThread = new Thread(this, DirectoryWatchService.class.getSimpleName());
+            runnerThread.start();
+        }
+    }
+
+    /**
+     * Stop this <code>SimpleDirectoryWatchService</code> thread. The killing happens lazily, giving the running thread an opportunity to finish the work at hand.
+     *
+     * @see #start()
+     */
+    @Override
+    public void stop() {
+        // Kill thread lazily
+        isRunning.set(false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        System.out.println("Starting file watcher service.");
+
+        while (isRunning.get()) {
+            WatchKey key;
+            try {
+                key = watchService.take();
+            } catch (InterruptedException e) {
+                System.out.println(
+                    DirectoryWatchService.class.getSimpleName()
+                    + " service interrupted."
+                );
+                break;
+            }
+
+            if (null == getDirPath(key)) {
+                System.out.println("Watch key not recognized.");
+                continue;
+            }
+
+            notifyListeners(key);
+
+            // Reset key to allow further events for this key to be processed.
+            boolean valid = key.reset();
+            if (!valid) {
+                watchKeyToDirPathMap.remove(key);
+                if (watchKeyToDirPathMap.isEmpty()) {
+                    break;
+                }
+            }
+        }
+
+        isRunning.set(false);
+        System.out.println("Stopping file watcher service.");
+    }
+}
